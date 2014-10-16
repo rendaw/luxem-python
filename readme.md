ee386716f9231d2b8e04b7c7d5abf9e87f56dd34
Rendaw <rendaw@zarbosoft.com>
2014-10-16 16:31:16 +0900

    Added readme, licence, fixed up setup.py.

---
+++ readme.md
@@ -0,0 +1,2 @@
+See the documentation [here](http://rendaw.github.io/luxem-python/index.html).
+
--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from distutils.core import setup, Extension
 
 setup(
-    name = 'PackageName', 
+    name = 'luxem', 
     version = '0.1', 
-    description = 'This is a demo package', 
+    description = 'Luxem is a typed data serialization format similar to JSON.', 
     author = 'Rendaw', 
     author_email = 'rendaw@zarbosoft.com', 
     url = 'http://www.zarbosoft.com/luxem', 
-    long_description = '''
-Luxem is a typed data serialization format similar to JSON.
-''',
+    long_description = 'Luxem is a typed data serialization format similar to JSON.',
     ext_modules = [
         Extension(
             '_luxem', 
             sources = [
                     '_luxem.c',
-                    '../c/luxem_rawread.c',
-                    '../c/luxem_rawwrite.c',
-                    '../c/luxem_internal_common.c',
+                    'c/luxem_rawread.c',
+                    'c/luxem_rawwrite.c',
+                    'c/luxem_internal_common.c',
             ]
         )
     ]
